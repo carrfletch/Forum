276a232f1ac1aebf9b7df454789984f17b437352
carrfletch <manuelcarr68@gmail.com>
2020-02-11 00:04:49 +0800

    index ,login and sign up Design

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Index</title>
+</head>
+
+<?php 
+
+include "header/header.php"
+
+?>
+
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">KSA</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <form method="GET" action="" class="input-group" style="padding-right: 10px">
+
+
+              <input type="text" name="Search" class="form-control" placeholder="Search for..." >
+
+              <span class="input-group-btn">
+                <button type="submit" name="searchBtn" class="btn btn-secondary" type="button">Go!</button>
+              </span>
+            </form>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="loginsystem/signup.php">Sign Up</a>
+          </li>
+          <li>
+          <a class="nav-link js-scroll-trigger"  href="loginsystem/login.php">Log In</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white">
+    <div class="container text-center">
+      <h1>Welcome to KSA Forum</h1>
+      <p class="lead">A place of discussion for KAL Skills Akademie Students</p>
+    </div>
+  </header>
+<?php include "header/footer.php"; ?>
+
+</body>
+</html>
---
+++ loginsystem/signup.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+
+<?php include "../header/header.php"; ?>
+
+
+<body class="loginSignupbody">
+
+
+<div id="login">
+        <h1 class="text-center text-white pt-5">Sign-Up</h1>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center align-content-center">
+                <div id="login-column" class="col-md-5">
+                    <div id="login-box" class="col-md-11" style="height: 500px;margin-top: 0px">
+                        <form id="login-form" class="form" action="" method="post">
+                            <!-- Display validation Error Here -->
+                          
+
+
+
+                            <div class="form-group">
+                                <label for="fullname" class="text-info">Full Name:</label><br>
+                                <input type="text" name="fullname" id="fullname" class="form-control" value="<?php echo $fullname; ?>">
+                            </div> <span class="error"><?php echo $fullnameErr; ?></span>
+
+                            <div class="form-group">
+                                <label for="email" class="text-info">Email:</label><br>
+                                <input type="text" name="email" id="email" class="form-control" value="<?php echo $email; ?>">
+                            </div>  <span class="error"><?php echo $emailErr; ?></span>
+
+                            <div class="form-group">
+                                <label for="username" class="text-info">Username:</label><br>
+                                <input type="text" name="username" id="username" class="form-control" value="<?php echo $username; ?>">
+                            </div> <span class="error"><?php echo $usernameErr; ?></span>
+
+                            <div class="form-group">
+                                <label for="password" class="text-info">Password:</label><br>
+                                <input type="password" name="password" id="password" class="form-control" value="<?php echo $password; ?>">
+                            </div> <span class="error"><?php echo $passwordErr; ?></span>
+
+                            <div class="form-group">
+                                <input type="submit" name="btnRegister" class="btn btn-info btn-md" value="Register"><br>
+                                <p>Are You Already Registered?  <a href="login.php" class="text-info">Sign-in</a> </p>
+                            </div>
+                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
