24a9d1ca20ea5daf053f87ae5cda71e161bcd4e8
carrfletch <manuelcarr78@gmail.com>
2020-02-14 01:25:03 +0800

    inner join , edit profile front and back end , create home costumized homepage and also create userposts

--- userdashboard/profile.php
+++ userdashboard/profile.php
@@ -15,7 +15,26 @@ if (isset($_SESSION["id"])) {
 
 ?>
 
+<?php 
+ include "../dbconnection/connection.php";
+$post = $postErr = "";
+if (isset($_POST["btnshare"])) 
+{
+  if (empty($_POST["post"])) {
+    $postErr = "This Field must not be Empty!";
+  }else {
+    $post = $_POST["post"];
+  }
+  if ($post) 
+  {
 
+    $sql = "INSERT INTO posts (post_description)
+          VALUES ('$post')";
+      mysqli_query($conn, $sql);
+  }
+}
+
+?>
 
 <!DOCTYPE html>
 <html>
@@ -114,39 +133,123 @@ if (isset($_SESSION["id"])) {
                 </div>
 
 
-                <?php 
-                include "../dbconnection/connection.php";
-                $result_posts = mysqli_query($conn, "SELECT * FROM posts INNER JOIN users ON posts.post_id = users.id ORDER BY post_id DESC ");
-
-                while ($row = mysqli_fetch_assoc($result_posts)) 
-                    
-                      
-
-                   
-                { ?>
-                
-                <div class="py-4">
-                    <h5 class="mb-3">Recent posts</h5>
-                    <div class="p-4 bg-light rounded shadow-sm">
-                      <h5 class="mb-3"> <?php echo $row['firstname'] ?></h5>
-                        <p class="font-italic mb-0"><?php echo $row['post_description'];?> </p>
-                        <ul class="list-inline small text-muted mt-3 mb-0">
-                            <li class="list-inline-item"><i class="fa fa-comment-o mr-2"></i>12 Comments</li>
-                            <li class="list-inline-item"><i class="fa fa-heart-o mr-2"></i>200 Likes</li>
+                <!--- \\\\\\\Post-->
+                <form method="post">
+                <div class="card gedf-card">
+                    <div class="card-header">
+                        <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="posts-tab" data-toggle="tab" href="#posts" role="tab" aria-controls="posts" aria-selected="true">Make
+                                    a publication</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="images-tab" data-toggle="tab" role="tab" aria-controls="images" aria-selected="false" href="#images">Images</a>
+                            </li>
                         </ul>
                     </div>
+                    <div class="card-body">
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="posts" role="tabpanel" aria-labelledby="posts-tab">
+                                <div class="form-group">
+                                    <label class="sr-only" for="message">post</label>
+                                    <textarea name="post" class="form-control" id="message" rows="3" placeholder="What are you thinking?"></textarea>
+                                    
+                                </div><span class="error"><?php echo $postErr; ?> </span>
+                                    
+
+                            </div>
+                            <div class="tab-pane fade" id="images" role="tabpanel" aria-labelledby="images-tab">
+                                <div class="form-group">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="customFile">
+                                        <label class="custom-file-label" for="customFile">Upload image</label>
+                                    </div>
+                                </div>
+                                <div class="py-4"></div>
+                            </div>
+                        </div>
+                        
+                        <div class="btn-toolbar justify-content-between">
+                            <div class="btn-group">
+                                <button type="submit" name="btnshare" class="btn btn-primary">share</button>
+                            </div>
+                            <div class="btn-group">
+                                <button id="btnGroupDrop1" type="button" class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
+                                    <i class="fa fa-globe"></i>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="btnGroupDrop1">
+                                    <a class="dropdown-item" href="#"><i class="fa fa-globe"></i> Public</a>
+                                    <a class="dropdown-item" href="#"><i class="fa fa-users"></i> Friends</a>
+                                    <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Just me</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+              </form>
+                <!-- Post /////-->
+
+                <?php 
+                include "../dbconnection/connection.php";
+                $view_posts = mysqli_query($conn, "SELECT * FROM posts INNER JOIN users ON posts.post_id = users.id ORDER BY post_id DESC ");
+
+                while ($row = mysqli_fetch_assoc($view_posts)) {
+                  ?>
                 
+            
+            </div>
 
+                <div class="card gedf-card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="mr-2">
+                                    <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt="">
+                                </div>
+                                <div class="ml-2">
+                                    <div class="h5 m-0"><?php echo $row['firstname'] ?></div>
+                                    <div class="h7 text-muted">Miracles Lee Cross</div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="dropdown">
+                                    <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-ellipsis-h"></i>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
+                                        <div class="h6 dropdown-header">Configuration</div>
+                                        <a class="dropdown-item" href="#">Save</a>
+                                        <a class="dropdown-item" href="#">Hide</a>
+                                        <a class="dropdown-item" href="#">Report</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                <?php } 
+                    </div>
+                    <div class="card-body">
+                        <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
+                        <a class="card-link" href="#">
+                            <h5 class="card-title">Lorem ipsum dolor sit amet, consectetur adip.</h5>
+                        </a>
+
+                        <p class="card-text">
+                            <?php echo $row['post_description'];?>
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
+                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
+                        <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
+                    </div>
+                </div>
+                <?php  
                    
-
+                    }
                 ?>
                 
-            </div>
-           
-        </div><!-- End profile widget -->
+                
 
     </div>
     </div>
--- userdashboard/profileaction.php
+++ userdashboard/profileaction.php
@@ -1,5 +1,5 @@
 <?php
 include "../dbconnection/connection.php";
 
-$result_posts =mysqli_query($conn, "SELECT * FROM `posts` INNER JOIN users ON posts.user_id = users.id ORDER BY post_id DESC ");
+$result_posts =mysqli_query($conn, "SELECT * FROM `posts` INNER JOIN users ON posts.id = users.post_id ORDER BY post_id DESC ");
 ?>
